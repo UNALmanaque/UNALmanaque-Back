6e7075270738891e80fd8760fb938dc51d133baf
Jherson Medina <admedinaco@unal.edu.co>
2021-04-12 16:49:22 -0500

    xd

--- backend/src/main/java/co/edu/una/UNalmanaqueback/backend/repository/CategoryRepository.java
+++ backend/src/main/java/co/edu/una/UNalmanaqueback/backend/repository/CategoryRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import co.edu.una.UNalmanaqueback.backend.model.Category;
 
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, Integer>{
 }
